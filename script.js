0012c44ac39ec7e9593d3b7ec407dc680571c73d
Jeffrey <lojeffreyou@gmail.com>
2021-02-24 23:40:46 -0500

    added plan for different calendar sorts

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 //We want to sort elements given their priority//
 
+//Ways to sort//
+
+//1) Sort by duration without alotted time
+//2) Sort by alotted time and percentage
+
+
 //Step 1: User enters items that they consider highest to lowest priority. The item is then array.push() into the corresponding array.
 
 var firstPriority = [
     {
         task: 'Work on the Odin Project',
-        duration: 3,
+        duration: 2,
     },
     {
         task:'Work on independent project',
@@ -23,13 +29,13 @@ var thirdPriority = [
     },
     {
         task: 'Work on Leet Code Questions',
-        duration: 1.5,
+        duration: 2,
     }
 ];
 var fourthPriority = [
     {
         task: 'Read "Learning SQL"',
-        duration: 1,
+        duration: 1.5,
     },
     {
         task: 'Watch video on Graph Theory',
@@ -37,4 +43,54 @@ var fourthPriority = [
     }
 ];
 
-//Step 2: User says how much time they allocate to each 
+var priorityList = [firstPriority, secondPriority, thirdPriority, fourthPriority];
+
+//Question about hoisting -> why do I have to assign the variables to priorityList after I declare the other variables?
+
+//Step 2: User says how much time they can allocate in a given day
+
+var alottedTime = 5;
+
+//Step 2a: Users can change the distribution percentage of how each group should be prioritized. They will enter a whole number like 60%, and then the program will covert to 0.60.
+
+//Example 1: 
+
+var firstPercentage = 0.60;
+var secondPercentage = 0.20;
+var thirdPercentage = 0.15;
+var fourthPercentage = 0.05;
+
+//Step 3: Program to break down your alotted time of your day to the given percentages
+
+var firstTime = alottedTime * firstPercentage;
+var secondTime = alottedTime * secondPercentage;
+var thirdTime = alottedTime * thirdPercentage;
+var fourthTime = alottedTime * fourthPercentage;
+
+// console.log(firstTime);
+// console.log(secondTime);
+// console.log(thirdTime);
+// console.log(fourthTime);
+
+//Step 4: Create Arrays with schedules
+
+    //First way of sorting
+    var schedule1 = [];
+
+    var durationSort = (arr) => {
+        arr.map(task =>
+            //Pick a random task inside each va
+            console.log(task[Math.floor(Math.random()*task.length)])
+        )
+    }
+
+    durationSort(priorityList)
+    
+
+
+
+/*************************************** IDEAS / QUESTIONS *******************************************
+ * 1) Have firstPriority, secondPriority, etc have prototype properties so that when a user presses a button or click-drag items, the program will automatically adjust percentages
+ * 
+ * 2) 
+ */
