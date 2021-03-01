2131c27419311b93249342b157fbea767eb5e926
Jeffrey Ou <lojeffreyou@gmail.com>
2021-02-28 21:58:15 -0500

    added comments for changes

--- script.js
+++ script.js
@@ -2,26 +2,31 @@
 
 //Ways to sort//
 
-//1) Sort by duration without alotted time
-//2) Sort by alotted time and percentage
+//1) Sort by duration without alotted time or percentage
+//2) Sort by alotted time and percentage (don't have durations)
 
 
 //Step 1: User enters items that they consider highest to lowest priority. The item is then array.push() into the corresponding array.
+
+var priorityList = [firstPriority, secondPriority, thirdPriority, fourthPriority];
 
 var firstPriority = [
     {
         task: 'Work on the Odin Project',
         duration: 2,
+        important: true,
     },
     {
         task:'Work on independent project',
         duration: 3,
     }
 ];
-var secondPriority = [{
+var secondPriority = [
+    {
     task: 'Read Algorithm Design Manual',
     duration: 2,
-}];
+    }
+];
 var thirdPriority = [
     {
         task: 'Work on Code Wars Questions', 
@@ -43,7 +48,7 @@ var fourthPriority = [
     }
 ];
 
-var priorityList = [firstPriority, secondPriority, thirdPriority, fourthPriority];
+console.log(priorityList)
 
 //Question about hoisting -> why do I have to assign the variables to priorityList after I declare the other variables?
 
@@ -53,7 +58,6 @@ var alottedTime = 5;
 
 //Step 2a: Users can change the distribution percentage of how each group should be prioritized. They will enter a whole number like 60%, and then the program will covert to 0.60.
 
-//Example 1: 
 
 var firstPercentage = 0.60;
 var secondPercentage = 0.20;
@@ -78,10 +82,21 @@ var fourthTime = alottedTime * fourthPercentage;
     var schedule1 = [];
 
     var durationSort = (arr) => {
-        arr.map(task =>
-            //Pick a random task inside each va
-            console.log(task[Math.floor(Math.random()*task.length)])
-        )
+        var temp = [];
+        //Pick a random task in each priority
+        arr.map(task => {
+            //Pick a random task inside each priority list
+            var pickedTask = task[Math.floor(Math.random()*task.length)];
+            console.log(pickedTask)
+            if(pickedTask.duration > 0) {
+                var multiplier = (pickedTask.duration / 0.5)
+                
+            }
+        
+        })
+
+        //separate the tasks into 30minute segments
+       
     }
 
     durationSort(priorityList)
@@ -92,5 +107,5 @@ var fourthTime = alottedTime * fourthPercentage;
 /*************************************** IDEAS / QUESTIONS *******************************************
  * 1) Have firstPriority, secondPriority, etc have prototype properties so that when a user presses a button or click-drag items, the program will automatically adjust percentages
  * 
- * 2) 
+ * 2) Have the User pick whether they want to work on task randomly or must do tasks (i.e. set a checkmark )
  */
