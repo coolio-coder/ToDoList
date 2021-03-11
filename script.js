0b7a7b7d6aa2083dc350b030422c913efdc84501
Jeffrey <lojeffreyou@gmail.com>
2021-03-11 00:27:09 -0500

    added Bulma

--- script.js
+++ script.js
@@ -89,31 +89,29 @@ var fourthTime = alottedTime * fourthPercentage;
         //Pick a random task in each priority
         for(let i=0;i<arr.length;i++) {
             var pickedTask = arr[i][Math.floor(Math.random()*arr[i].length)];
-            console.log(pickedTask)
             //separate the tasks into 30minute segments
             var multiplier = (pickedTask.duration / 0.5)
-            console.log(multiplier)
             for(let j=0;j<multiplier;j++) {
                 const newMiniTask = Object.assign({}, pickedTask, { duration: 0.5 });
                 schedule.push(newMiniTask);
             }
         }
-        console.log(schedule[0])
-        console.log(schedule[1])
-        console.log(schedule[2])
-        console.log(schedule[3])
-        console.log(schedule[4])
-        console.log(schedule[5])
-        console.log(schedule)
+        // console.log(schedule[0])
+        // console.log(schedule[1])
+        // console.log(schedule[2])
+        // console.log(schedule[3])
+        // console.log(schedule[4])
+        // console.log(schedule[5])
+        var importantTasks = [];
         //sorting the schedule based on importance
         for(let k=0;k<schedule.length;k++){
+            console.log(schedule[k])
             if(schedule[k].important === true) {
-                schedule1.push(schedule[k]);
+                importantTasks.push(schedule[k]);
                 schedule.splice(k,1)
             }
         }
-        console.log(schedule1)
-        console.log(schedule)
+        console.log(importantTasks)
         //Fisher-Yates Shuffle https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
         function shuffle (array) {
             var currentIndex=array.length, temporaryValue, randomIndex;
@@ -133,18 +131,16 @@ var fourthTime = alottedTime * fourthPercentage;
 
             return array;
         }
-        return schedule1.concat(shuffle(schedule));
-        //return [importantTasks, unimportantShuffledTasks]
+        var shuffledTasks = shuffle(schedule);
+        return [importantTasks,shuffledTasks];
 }
 //question about scoping inside for loop and outside, can you use the same variable?
-var tasksList = durationSort(priorityList);
+durationSort(priorityList)
 
 
 //Add task options
 
 function addTask () {
-
-    //Create Elements
     const div = document.createElement('div');
     const taskInput = document.createElement('input');
     const durationInput = document.createElement('input');
@@ -174,7 +170,7 @@ function addTask () {
     addTaskbutton.id='a-priority';
     addTaskbutton.innerHTML='+ Add more Task'
 
-        //Append it to the array
+    console.log(event)
         document.getElementById(event.srcElement.parentNode.id).appendChild(taskInput);
         document.getElementById(event.srcElement.parentNode.id).appendChild(linebreak);
     
@@ -191,18 +187,50 @@ function addTask () {
     
 }
 
-function createSchedule () {
-    //loop 
-    const div = document.createElement('div');
-    for(let i=0;i<schedule1.length;i++){
-        div.innerHTML='';
-        console.log(schedule1[i].task)
-        div.innerHTML = schedule1[i].task;
-        document.getElementById('yourSchedule').appendChild(div);
+window.addEventListener('load', (event) => {
+    var randomSort = durationSort(priorityList);
+    for(let i=0; i<randomSort.length;i++) {
+        for(let j=0; j<randomSort[i].length;j++){
+            console.log(randomSort[i][j])
+        }
     }
+})
+
+
+
+
+
+
+//Carousel script
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
 
-window.onload = createSchedule;
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
 
 /*************************************** IDEAS / QUESTIONS *******************************************
  * 1) How do we come up with a way to effectively read whick task number it is
