c3b4619bb40d5567307cf361bf38820e0c437530
Jeffrey <lojeffreyou@gmail.com>
2021-04-18 16:26:55 -0400

    added sass files to updatte the navbar

--- script.js
+++ script.js
@@ -863,3 +863,29 @@ var newArray = [].concat(array[0],array[1])
 console.log(newArray)
 
 // sessionStorage.setItem("tasks", JSON.stringify([].concat(finalTaskList[0],finalTaskList[1])));
+
+
+let findPermutations = (string) => {
+    if (!string || typeof string !== "string"){
+      return "Please enter a string"
+    } else if (string.length < 2 ){
+      return string
+    }
+  
+    let permutationsArray = [] 
+     
+    for (let i = 0; i < string.length; i++){
+      let char = string[i]
+  
+      if (string.indexOf(char) != i)
+      continue
+  
+      let remainingChars = string.slice(0, i) + string.slice(i + 1, string.length)
+  
+      for (let permutation of findPermutations(remainingChars)){
+        permutationsArray.push(char + permutation) }
+    }
+    return permutationsArray
+  }
+
+  console.log(findPermutations('aab'))
